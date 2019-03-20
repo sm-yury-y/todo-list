c0f01e4c0a2b94e7be810b2403642cb3085fc95b
sm-yury-y <sm.yuri-y@yandex.ru>
2019-03-20 23:36:15 +0300

    Изменяет TodoListItem и ItemStatusFilter c компонентов-функций на компоненты-классы

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,9 +10,9 @@ import './App.css';
 const App = () => {
 
   const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
+    { label: 'Выпить кофе', important: false, id: 1 },
+    { label: 'Почитать книгу', important: true, id: 2 },
+    { label: 'Пообедать', important: false, id: 3 }
   ];
 
   return (
--- src/components/ItemStatusFilter/ItemStatusFilter.js
+++ src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './ItemStatusFilter.css';
 
-const ItemStatusFilter = () => {
-  return (
-    <div className="btn-group">
-      <button type="button"
-              className="btn btn-info">Все</button>
-      <button type="button"
-              className="btn btn-outline-secondary">Активные</button>
-      <button type="button"
-              className="btn btn-outline-secondary">Завершенные</button>
-    </div>
-  );
-};
 
-export default ItemStatusFilter;
+export default class ItemStatusFilter extends Component {
+  render() {
+    return (
+      <div className="btn-group">
+        <button type="button"
+                className="btn btn-info">Все</button>
+        <button type="button"
+                className="btn btn-outline-secondary">Активные</button>
+        <button type="button"
+                className="btn btn-outline-secondary">Завершенные</button>
+      </div>
+    );
+  }
+}
