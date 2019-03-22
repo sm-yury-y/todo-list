f3a811cd1ff7fbd5fa1ae91e5d736267fb0b0da2
sm-yury-y <sm.yuri-y@yandex.ru>
2019-03-22 21:15:13 +0300

    Добавляет возможность удаления элемента из списка

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../AppHeader';
 import SearchPanel from '../SearchPanel';
@@ -7,25 +7,45 @@ import ItemStatusFilter from '../ItemStatusFilter';
 
 import './App.css';
 
-const App = () => {
+export default class App extends Component {
 
-  const todoData = [
-    { label: 'Выпить кофе', important: false, id: 1 },
-    { label: 'Почитать книгу', important: true, id: 2 },
-    { label: 'Пообедать', important: false, id: 3 }
-  ];
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  state  = {
+    todoData: [
+      { label: 'Выпить кофе', important: false, id: 1 },
+      { label: 'Почитать книгу', important: true, id: 2 },
+      { label: 'Пообедать', important: false, id: 3 }
+    ]
+  };
+
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      const newArray = [
+        ...todoData.slice(0, idx),
+        ...todoData.slice (idx + 1)
+      ];
+
+      return {
+        todoData: newArray
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList
+          todos={this.state.todoData}
+          onDeleted={ this.deleteItem }/>
       </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
+    );
+  }
 };
-
-export default App;
