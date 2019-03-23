4d6fccea7b7c2c808fda1256e671b1a24e62f1aa
sm-yury-y <sm.yuri-y@yandex.ru>
2019-03-23 23:01:28 +0300

    Реализует функционал добавления элемента в список

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,11 +4,13 @@ import AppHeader from '../AppHeader';
 import SearchPanel from '../SearchPanel';
 import TodoList from '../TodoList';
 import ItemStatusFilter from '../ItemStatusFilter';
+import ItemAddForm from '../ItemAddForm';
 
 import './App.css';
 
 export default class App extends Component {
 
+  maxId = 100;
 
   state  = {
     todoData: [
@@ -33,6 +35,26 @@ export default class App extends Component {
     });
   };
 
+  addItem = (text) => {
+    const newItem = {
+      label: text,
+      important:false,
+      id: this.maxId++
+    };
+
+    this.setState(({todoData}) => {
+      const newArr = [
+        ...todoData,
+        newItem
+      ];
+
+      return {
+        todoData: newArr
+      };
+
+    });
+  };
+
   render() {
     return (
       <div className="todo-app">
@@ -45,6 +67,7 @@ export default class App extends Component {
         <TodoList
           todos={this.state.todoData}
           onDeleted={ this.deleteItem }/>
+          <ItemAddForm onItemAdded={this.addItem}/>
       </div>
     );
   }
