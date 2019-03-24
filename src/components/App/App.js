0fc2f5ad76649a17acad3f21b1d397eca9d5b26b
sm-yury-y <sm.yuri-y@yandex.ru>
2019-03-24 14:02:22 +0300

    Добавляет счетчик завершенных дел

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,10 +14,19 @@ export default class App extends Component {
 
   state  = {
     todoData: [
-      { label: 'Выпить кофе', important: false, id: 1 },
-      { label: 'Почитать книгу', important: true, id: 2 },
-      { label: 'Пообедать', important: false, id: 3 }
+      this.createTodoItem('Выпить кофе'),
+      this.createTodoItem('Почитать книгу'),
+      this.createTodoItem('Пообедать')
     ]
+  };
+
+  createTodoItem (label) {
+    return {
+      label,
+      important:false,
+      done:false,
+      id: this.maxId++
+    };
   };
 
   deleteItem = (id) => {
@@ -36,11 +45,7 @@ export default class App extends Component {
   };
 
   addItem = (text) => {
-    const newItem = {
-      label: text,
-      important:false,
-      id: this.maxId++
-    };
+    const newItem = this.createTodoItem(text);
 
     this.setState(({todoData}) => {
       const newArr = [
@@ -55,18 +60,56 @@ export default class App extends Component {
     });
   };
 
+  toggleProperty  (arr, id, propName)  {
+    const idx = arr.findIndex((el) => el.id === id);
+
+    const oldItem = arr[idx];
+    const newItem = {...oldItem, [propName]: !oldItem[propName]};
+
+    return [
+      ...arr.slice(0, idx),
+         newItem,
+      ...arr.slice (idx + 1)
+    ];
+  };
+
+  onToggleDone = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, 'done')
+      };
+    });
+  };
+
+  onToggleImportant = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, 'important')
+      };
+    });
+  };
+
   render() {
+
+    const { todoData } = this.state;
+
+    const doneCount = todoData.filter((el) => el.done).length;
+    const todoCount = todoData.length - doneCount;
+
     return (
       <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
+        <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
           <SearchPanel />
           <ItemStatusFilter />
         </div>
 
         <TodoList
-          todos={this.state.todoData}
-          onDeleted={ this.deleteItem }/>
+          todos={todoData}
+          onDeleted={this.deleteItem}
+          onToggleImportant={this.onToggleImportant}
+          onToggleDone={this.onToggleDone} />
+
           <ItemAddForm onItemAdded={this.addItem}/>
       </div>
     );
--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -5,7 +5,7 @@ const AppHeader = ({toDo, done}) => {
   return (
     <div className="app-header d-flex">
       <h1>Список дел</h1>
-      <h2>Еще {toDo} дело, {done} завершено</h2>
+      <h2>Еще дел {toDo} , {done} завершено</h2>
     </div>
   );
 };
