88848924ab76ff70be2da7c60afd34e6b1e6a3e4
sm-yury-y <sm.yuri-y@yandex.ru>
2019-03-26 22:43:28 +0300

    Добавляет input для ввода дел в список

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -4,12 +4,37 @@ import './ItemAddForm.css';
 
 export default class ItemAddForm extends Component {
 
+state = {
+  label: ''
+};
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+    this.setState ({
+      label: ''
+    });
+  };
+
   render() {
     return (
-      <div className="item-add-form">
-        <button className="btn btn-outline-secondary"
-                onClick={() => this.props.onItemAdded('Привет')} >Добавить в список</button>
-      </div>
+      <form className="item-add-form  d-flex"
+            onSubmit={this.onSubmit}>
+
+        <input type="text"
+               className="form-control"
+               onChange={this.onLabelChange}
+               placeholder="Что нужно сделать"
+               value={this.state.label}/>
+
+        <button className="btn btn-outline-secondary">Добавить в список</button>
+      </form>
     )
   }
 
